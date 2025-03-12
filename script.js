f585c9f71e20bcc985389fda1e306132d10707ab
whiteamou <77854869@mail.ru>
2025-03-12 23:04:24 +0200

    button like on card and  functional on its button

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const editPopup = document.querySelector('.nav__edit_btn');
 const popup = document.getElementById('popup-edit-profile');
 const closeButton = document.getElementById('closePopup');
 const saveButton=document.getElementById('saveBtn');
-const likeBtns=document.querySelectorAll('.card__btn_like');
 
 // Открытие поп-апа
 const createPopup = () => {
@@ -10,17 +9,18 @@ const createPopup = () => {
 document.body.classList.add('body_opened');
 };
 
+if(editPopup) editPopup.addEventListener('click', createPopup);
+
 // Закрытие поп-апа
-closeButton.addEventListener('click', () => {
+if(closeButton) closeButton.addEventListener('click', () => {
   popup.classList.remove('popup_opened');
   document.body.classList.remove('body_opened');
 });
 
-// Открытие поп-апа при клике на кнопку редактирования
-editPopup.addEventListener('click', createPopup);
 
 
-saveButton.addEventListener('click', ()=>{
+
+if(saveButton) saveButton.addEventListener('click', ()=>{
   const name=document.getElementById('name').value;
   const description= document.getElementById('description').value;
 
@@ -29,12 +29,30 @@ saveButton.addEventListener('click', ()=>{
 
   popup.classList.remove('popup_opened');
   document.body.classList.remove('body_opened');
-})
-likeBtns.forEach( button => {
-  button.addEventListener('click',(event)=>{
-    const clickedButton= event.target;
-    clickedButton.classList.toogle('liked');
+});
 
+const likeBtns=document.querySelectorAll('.card__btn_like');
 
-  })
-})
+likeBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('liked');
+
+    const img=button.querySelector('.like__card_img');
+    if(img){
+      img.src=button.classList.contains('liked')
+      ? './logos/Like_black.svg'
+      : './logos/Like.svg'
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".card__btn_delete").forEach(button => {
+    button.addEventListener("click", () => {
+      const card = button.closest(".card");
+      if (card) {
+        card.remove(); // Удаляем карточку из DOM
+      }
+    });
+  });
+});
